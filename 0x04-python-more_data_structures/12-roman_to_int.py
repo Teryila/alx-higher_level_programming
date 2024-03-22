28441c4a431874b5acf5d855a2fcfeeab85d2887
Teryila <107785340+Teryila@users.noreply.github.com>
2024-03-22 04:07:56 -1100

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,34 +1,45 @@
-def roman_to_int(roman_string):
-	if not isinstance(roman_string, str) or roman_string is None:
-	return 0
+#include <Python.h>
+#include <stdio.h>
 
-	def to_subtract(list_num):
-	to_sub = 0
-	max_list = max(list_num)
+void print_python_bytes(PyObject *p) {
+    printf("[.] bytes object info\n");
 
-	for n in list_num:
-	if max_list > n:
-		to_sub += n
+    if (!PyBytes_Check(p)) {
+        printf("  [ERROR] Invalid Bytes Object\n");
+        return;
+    }
 
-	return (max_list - to_sub)
+    Py_ssize_t size = PyBytes_Size(p);
+    unsigned char *bytes_data = (unsigned char *)PyBytes_AsString(p);
 
-	rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-	list_keys = list(rom_n.keys())
+    printf("  size: %ld\n", size);
+    printf("  first %ld bytes: ", size > 10 ? 10 : size);
 
-	num = 0
-	last_rom = 0
-	list_num = [0]
+    for (Py_ssize_t i = 0; i < (size > 10 ? 10 : size); ++i) {
+        printf("%02x ", bytes_data[i]);
+    }
 
-	for ch in roman_string:
-	for r_num in list_keys:
-		if r_num == ch:
-		if rom_n.get(ch) <= last_rom:
-			num += to_subtract(list_num)
-			list_num = [rom_n.get(ch)]
-		else:
-			list_num.append(rom_n.get(ch))
+    printf("\n");
+}
 
-			last_rom = rom_n.get(ch)
-			num += to_subtract(list_num)
+void print_python_list(PyObject *p) {
+    printf("[*] Python list info\n");
 
-	return num
+    if (!PyList_Check(p)) {
+        printf("[ERROR] Invalid Python list object\n");
+        return;
+    }
+
+    Py_ssize_t size = PyList_Size(p);
+    printf("[*] Size of the Python List = %ld\n", size);
+
+    PyObject *item;
+    for (Py_ssize_t i = 0; i < size; ++i) {
+        item = PyList_GetItem(p, i);
+        printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+
+        if (PyBytes_Check(item)) {
+            print_python_bytes(item);
+        }
+    }
+}
